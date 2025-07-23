a374444ec32fe3f42d525b4b2ceaa507f3497e61
Haider Ali <connecthaiderali@gmail.com>
2025-07-23 17:13:47 +0500

    Issue in GYMmanagement Resolved

---
+++ SprigSecurity-Google/src/main/java/com/redmath/SprigSecurity/Google/SprigSecurityGoogleApplication.java
@@ -0,0 +1,11 @@
+package com.redmath.SprigSecurity.Google;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprigSecurityGoogleApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SprigSecurityGoogleApplication.class, args);
+    }
+}
---
+++ SprigSecurity-Google/src/test/java/com/redmath/SprigSecurity/Google/SprigSecurityGoogleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.redmath.SprigSecurity.Google;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprigSecurityGoogleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
